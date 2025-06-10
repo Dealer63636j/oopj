fbad8a205ca6b31218cd71a626ec0894d4fd3a7d
Dealer63636j <dealersausage@gmail.com>
2025-06-10 11:57:33 +0530

    LAB

---
+++ lab 1/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    printf("Give three number");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("Avg =%f", ((a + b + c) / 3));
+}
---
+++ lab 4/circle.java
@@ -0,0 +1,6 @@
+class circle{
+	public static void main(String[] args){
+		int r=Integer.parseInt(args[0]);
+		System.out.println("The sum of three numbers is: " + (3.14*r*r));
+	}
+}
